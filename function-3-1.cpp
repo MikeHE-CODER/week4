fb55889ebf8ec182683ea153383e3334b102e40f
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-11 18:35:12 +0930

    Update function-3-1.cpp

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -7,7 +7,11 @@ int *readNumbers() {
   }
   return array;
 }
-
+void printNumbers(int *numbers, int length) {
+  for (int i = 0; i < length; i++) {
+    cout << i << ' ' << numbers[i] << endl;
+  }
+}
 bool equalsArray(int *numbers1, int *numbers2, int length) {
   for (int i = 0; i < length; i++) {
     if (numbers1[i] != numbers2[i]) {
