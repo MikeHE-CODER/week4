af494f428c9dccf6c58fb503440e37a1770ec2a0
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-11 18:06:13 +0930

    Create main-3-1.cpp

---
+++ main-3-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+extern int *readNumbers();
+extern bool equalsArray(int *numbers1, int *numbers2, int length);
+using namespace std;
+int main() {
+  int *numbers1 = readNumbers();
+  int *numbers2 = readNumbers();
+  cout << equalsArray(numbers1, numbers2, 10);
+}
